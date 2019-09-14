e3011dec4e2a718c056ca0b1ab22c4e2774415be
David Ruhe <davidr@int2.bullx>
2019-09-14 17:40:30 +0200

    working pgan files

---
+++ pgan/network.py
@@ -0,0 +1,232 @@
+import tensorflow as tf
+from tensorflow.python import keras
+from tensorflow.python.keras.layers.merge import _Merge
+from tensorflow.python.keras import layers
+import math
+import horovod.tensorflow as hvd
+
+from loss import wasserstein_loss, gradient_penalty_loss
+
+def num_filters(phase, num_phases, base_dim):
+    num_downscales = int(math.log2(base_dim / 16))
+    filters = min(base_dim // (2 ** (phase - num_phases + num_downscales)), base_dim)
+    return filters
+
+
+class GeneratorBlock(keras.Sequential):
+    def __init__(self, filters, **kwargs):
+        super(GeneratorBlock, self).__init__(**kwargs)
+        self.add(layers.UpSampling3D())
+        self.add(layers.Convolution3D(filters, (3, 3, 3), padding='same'))
+        self.add(layers.LeakyReLU())
+
+        # self.add(layers.Convolution3D(filters, (3, 3, 3), padding='same'))
+        # self.add(layers.LeakyReLU())
+        
+        # self.add(layers.Convolution3D(filters, (3, 3, 3), padding='same'))
+        # self.add(layers.LeakyReLU())
+        
+
+def make_generator(phase, num_phases, base_dim, latent_dim):
+        
+    z = layers.Input((latent_dim,), name='generator_input')
+    alpha_in = layers.Input((1,), name='generator_mixing_parameter')
+    
+    filters = num_filters(0, num_phases, base_dim)
+    
+    x = keras.Sequential((
+        layers.Dense(4 * 4 * 4 * filters),
+        layers.LeakyReLU(),
+        layers.Reshape((4, 4, 4, filters)),
+        # layers.Convolution3D(filters, (3, 3, 3), padding='same'),
+        # layers.LeakyReLU(),
+        # layers.Convolution3D(filters, (3, 3, 3), padding='same'),
+        # layers.LeakyReLU()
+    ), name='generator_in')(z)
+    
+    x_upsampled = None  # Placeholder
+    for i in range(1, phase):
+        
+        if i == phase - 1:
+            x_upsampled = layers.LeakyReLU()(layers.Conv3D(
+                1, 1, name=f'to_rgb_{phase - 1}')(layers.UpSampling3D()(x)))
+        
+        filters = num_filters(i, num_phases, base_dim)
+        x = GeneratorBlock(filters=filters, name=f'generator_block_{i}')(x)
+            
+    x = layers.Conv3D(1, 1, name=f'to_rgb_{phase}')(x)
+    
+    if x_upsampled is not None:
+        x = AlphaMixingLayer()([x_upsampled, x, alpha_in])
+    
+    model = keras.Model(inputs=((z, alpha_in)), outputs=(x))
+    return model
+        
+        
+class DiscriminatorBlock(keras.Sequential):
+    def __init__(self, filters, **kwargs):
+        super(DiscriminatorBlock, self).__init__(**kwargs)
+        # self.add(layers.Conv3D(filters, 3, padding='same'))
+        # self.add(layers.LeakyReLU())
+        
+        # self.add(layers.Conv3D(filters, 3, padding='same'))
+        # self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv3D(filters, 3, padding='same'))
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.AveragePooling3D())
+
+
+def make_discriminator(phase, num_phases, base_dim, img_shape, latent_dim):
+    
+    z = layers.Input(img_shape, name='discriminator_input')
+    alpha = layers.Input((1,), name='discriminator_mixing_parameter')
+        
+    filters = num_filters(phase - 1, num_phases, base_dim)
+    x = layers.Conv3D(filters, 1, name=f'from_rgb_{phase}')(z)
+    x = layers.LeakyReLU()(x)
+    
+
+    for i in reversed(range(0, phase - 1)):
+        filters = num_filters(i, num_phases, base_dim)
+        
+        x = DiscriminatorBlock(filters=filters,
+                               name=f'discriminator_block_{i + 1}')(x)
+        
+        if i == phase - 2:
+            x_downscaled = layers.LeakyReLU()(layers.Conv3D(filters, 1, name=f'from_rgb_{phase - 1}')(layers.AveragePooling3D()(z)))
+            x = AlphaMixingLayer()([x_downscaled, x, alpha])
+    
+    x = keras.Sequential((
+        # layers.Conv3D(filters, (3, 3, 3), padding='same'), 
+        # layers.LeakyReLU(),
+        # layers.Conv3D(filters, (3, 3, 3), padding='same'), 
+        # layers.LeakyReLU(),
+        layers.Flatten(),
+        layers.Dense(latent_dim),
+        layers.LeakyReLU(),
+        layers.Dense(1),
+    ), name='discriminator_out')(x)
+
+    
+    model = keras.Model(inputs=(z, alpha), outputs=(x,))
+    return model
+
+
+class RandomWeightedAverage(_Merge):
+    """Takes a randomly-weighted average of two tensors. In geometric terms, this
+    outputs a random point on the line between each pair of input points.
+    Inheriting from _Merge is a little messy but it was the quickest solution I could
+    think of. Improvements appreciated."""
+
+    def _merge_function(self, inputs):
+        weights = tf.random.normal((inputs[0].shape[0], 1, 1, 1, 1))
+        return (weights * inputs[0]) + ((1 - weights) * inputs[1])
+    
+    
+class AlphaMixingLayer(_Merge):
+    def _merge_function(self, inputs):
+        alpha = inputs[-1]
+        return (alpha * inputs[0]) + ((1 - alpha) * inputs[1])
+
+
+def get_training_functions(horovod):
+    @tf.function
+    def train_discriminator(generator, 
+                            discriminator, 
+                            discriminator_optim, 
+                            batch_size, 
+                            latent_dim, 
+                            x_real, 
+                            alpha, 
+                            gradient_penalty_weight,
+                            is_first_batch,
+                            horovod=horovod):
+
+        for layer in discriminator.layers:
+            layer.trainable = True
+        discriminator.trainable = True
+
+        for layer in generator.layers:
+            layer.trainable = False
+        generator.trainable = False
+
+        with tf.GradientTape() as tape:
+            z = tf.random.normal(shape=(batch_size, latent_dim))
+            d_real = discriminator([x_real, alpha])
+            x_fake = generator([z, alpha])
+            d_fake = discriminator([x_fake, alpha])
+
+
+            averaged_samples = RandomWeightedAverage()([x_real, x_fake])
+
+            d_avg = discriminator([averaged_samples, alpha])
+
+            real_loss = wasserstein_loss(d_real)
+            fake_loss = wasserstein_loss(d_fake)        
+            gp_loss = gradient_penalty_loss(d_avg,
+                                 averaged_samples,
+                                 gradient_penalty_weight)
+
+            d_loss = real_loss - fake_loss + gp_loss
+            
+        if horovod:
+            tape = hvd.DistributedGradientTape(tape)
+
+        grads = tape.gradient(d_loss, discriminator.trainable_variables)
+        discriminator_optim.apply_gradients(zip(grads, discriminator.trainable_variables))
+
+        if horovod and is_first_batch:
+            if hvd.rank() == 0:
+                print("Broacasting discriminator variables.")
+            hvd.broadcast_variables(discriminator.variables, root_rank=0)
+            hvd.broadcast_variables(discriminator_optim.variables(), root_rank=0)
+        
+        return d_loss
+    
+    @tf.function
+    def train_generator(generator, 
+                        discriminator, 
+                        generator_optim, 
+                        batch_size, 
+                        latent_dim, 
+                        alpha, 
+                        is_first_batch,
+                        horovod=horovod):
+
+        for layer in discriminator.layers:
+            layer.trainable = False
+        discriminator.trainable = False
+
+        for layer in generator.layers:
+            layer.trainable = True
+        generator.trainable = True
+
+        with tf.GradientTape() as tape:
+            z = tf.random.normal(shape=(batch_size, latent_dim))
+            x_fake = generator([z, alpha])
+            d_fake = discriminator([x_fake, alpha])
+
+            g_loss = wasserstein_loss(d_fake)
+
+            
+        if horovod:
+            tape = hvd.DistributedGradientTape(tape)
+        
+        grads = tape.gradient(g_loss, generator.trainable_variables)
+        generator_optim.apply_gradients(zip(grads, generator.trainable_variables))
+        
+        if horovod and is_first_batch:
+            if hvd.rank() == 0:
+                print("Broadcasting generator variables.")
+            hvd.broadcast_variables(generator.variables, root_rank=0)
+            hvd.broadcast_variables(generator_optim.variables(), root_rank=0)
+
+        return g_loss, x_fake
+    
+    return train_generator, train_discriminator
+
+
+
+    
---
+++ pgan/train.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+from datetime import datetime
+import os
+import tensorflow as tf
+import pickle
+import numpy as np
+import horovod.tensorflow as hvd
+# from tensorflow.python.eager import profiler
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+for i in range(len(physical_devices)):
+    tf.config.experimental.set_memory_growth(physical_devices[i], True)
+
+from network_hvd import make_generator, make_discriminator, get_training_functions
+from utils import load_lidc_idri_dataset_from_tfrecords, generate_gif, print_gpu_info, print_cpu_info
+from loss import wasserstein_loss, gradient_penalty_loss
+
+
+def train(generator, 
+          discriminator,
+          generator_optim,
+          discriminator_optim,
+          train_generator,
+          train_discriminator,
+          latent_dim,
+          dataset,
+          mixing_epochs,
+          stabilizing_epochs,
+          epoch_size,
+          batch_size,
+          training_ratio,
+          timestamp,
+          gradient_penalty_weight,
+          summary_writer,
+          is_mixing,
+          horovod=False):
+    
+    if is_mixing:
+        c_alpha = 1.0
+        epoch_range = range(mixing_epochs)
+    else:
+        c_alpha = 0.0
+        epoch_range = range(mixing_epochs, mixing_epochs + stabilizing_epochs)
+        
+    for epoch in epoch_range:
+        if (horovod and hvd.rank() == 0) or not horovod:
+            print("Epoch: ", epoch)
+            
+        discriminator_loss = []
+        generator_loss = []    
+        
+        alpha = tf.fill((batch_size, 1, 1, 1, 1), c_alpha)
+        
+        if horovod:
+            iterator = dataset.take(max(1, epoch_size // hvd.size())) 
+        else:
+            iterator = dataset.take(epoch_size) 
+            
+        for i, image_batch in enumerate(iterator):
+            
+            is_first_batch = (i == 0) and (epoch == 0)
+
+            start = datetime.now()
+            d_loss = train_discriminator(generator, 
+                                         discriminator, 
+                                         discriminator_optim, 
+                                         batch_size, 
+                                         latent_dim, 
+                                         image_batch, 
+                                         alpha, 
+                                         gradient_penalty_weight,
+                                         is_first_batch)
+            
+            discriminator_loss.append(d_loss)
+
+            if i % training_ratio == 0:
+                g_loss, x_fake = train_generator(
+                    generator,
+                    discriminator,
+                    generator_optim,
+                    batch_size,
+                    latent_dim,
+                    alpha,
+                    is_first_batch
+                )
+                generator_loss.append(g_loss)
+            
+            end = datetime.now()
+            
+            if (horovod and hvd.rank() == 0) or not horovod:
+                print(f"Processed {image_batch.shape[0]} images in {(end - start).total_seconds()} seconds")
+                
+        if epoch % 1 == 0:
+            if (horovod and hvd.rank() == 0) or not horovod:
+                num_images = int(10 - np.log2(image_batch.shape[1])) ** 2
+                random_indices = np.random.choice(image_batch.shape[0], num_images)
+                originals = image_batch.numpy()
+                fakes = x_fake.numpy()
+                image_dir = os.path.join('logs', timestamp)
+                original_save_dir = os.path.join(image_dir, 'originals')
+                fake_save_dir = os.path.join(image_dir, 'fakes')
+                if not os.path.exists(original_save_dir):
+                    os.makedirs(original_save_dir)
+                if not os.path.exists(fake_save_dir):
+                    os.makedirs(fake_save_dir)
+                np.save(os.path.join(original_save_dir, f'{epoch}.npy'), originals)
+                np.save(os.path.join(fake_save_dir, f'{epoch}.npy'), fakes)
+                generate_gif(fakes[random_indices].squeeze(), originals[random_indices].squeeze(), image_dir, epoch)
+                
+                with summary_writer.as_default():
+                    original = originals[0]
+                    fake = fakes[0]
+                    tf.summary.image("Original example.", original, max_outputs=original.shape[0], step=epoch)
+                    tf.summary.image("Fake example.", original, max_outputs=fake.shape[0], step=epoch)
+
+        if is_mixing:
+            c_alpha -= 1 / mixing_epochs
+
+    return generator, discriminator
+
+def main(args):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    log_dir = os.path.join('logs', timestamp, 'summaries')
+    summary_writer = tf.summary.create_file_writer(log_dir)
+    
+    if (args.horovod and hvd.rank() == 0) or not args.horovod:
+        print(f"Number of phases is {args.num_phases}, final output resolution will be {2 * 2 ** args.num_phases}")
+    
+    for phase in range(1, args.num_phases + 1):
+        
+        if (args.horovod and hvd.rank() == 0) or not args.horovod:
+            print(f'\n|\t\t\tPhase {phase}\t\t\t|\n')
+        
+        size = 2 * 2 ** phase
+        tfr_path = args.tfrecords_path + f'tfrecords_new_{size}x{size}x{size}/'
+        
+        if not os.path.exists(tfr_path):
+            raise ValueError(f"Path doesn't exist: {tfr_path}")
+        
+        dataset_size = len(os.listdir(tfr_path))
+        shape = (size, size, size, 1)
+        
+        batch_size = 512 // size
+        dataset = load_lidc_idri_dataset_from_tfrecords(tfr_path, batch_size=batch_size, shape=shape)
+        epoch_size = dataset_size // batch_size
+        
+        generator = make_generator(phase, args.num_phases, args.base_dim, args.latent_dim)
+        discriminator = make_discriminator(phase, args.num_phases, args.base_dim, shape, args.latent_dim)
+        
+        if (args.horovod and hvd.rank() == 0) or not args.horovod:
+            print(generator.summary())
+            print(discriminator.summary())
+        
+        if args.horovod:
+        
+            generator_optim = tf.optimizers.Adam(1e-5 * hvd.size(), beta_1=0.5, beta_2=0.9)
+            discriminator_optim = tf.optimizers.Adam(1e-5 * hvd.size(), beta_1=0.5, beta_2=0.9)
+            generator_optim = hvd.DistributedOptimizer(generator_optim)
+            discriminator_optim = hvd.DistributedOptimizer(discriminator_optim)
+        else:
+            generator_optim = tf.optimizers.Adam(1e-3, beta_1=0.5, beta_2=0.9)
+            discriminator_optim = tf.optimizers.Adam(1e-3, beta_1=0.5, beta_2=0.9)
+             
+        train_generator, train_discriminator = get_training_functions(args.horovod)
+        
+        checkpoint_path_prev = os.path.join('checkpoints', f'phase_{phase - 1}')
+        if os.path.exists(checkpoint_path_prev):
+            if (args.horovod and hvd.rank() == 0) or not args.horovod:
+                print(f"Loading weights from phase {phase - 1} from {checkpoint_path_prev}")
+                generator.load_weights(os.path.join(checkpoint_path_prev, 'generator.h5'), by_name=True)
+                discriminator.load_weights(os.path.join(checkpoint_path_prev, 'discriminator.h5'), by_name=True)
+        
+        if (args.horovod and hvd.rank() == 0) or not args.horovod:
+            print("\n\t\t\tStarting mixing epochs\t\t\t\n")
+            
+        generator, discriminator = train(
+            generator,
+            discriminator,
+            generator_optim,
+            discriminator_optim,
+            train_generator,
+            train_discriminator,
+            args.latent_dim,
+            dataset,
+            args.mixing_epochs,
+            args.stabilizing_epochs,
+            epoch_size,
+            batch_size,
+            args.training_ratio,
+            timestamp,
+            args.gradient_penalty_weight,
+            summary_writer,
+            is_mixing=True,
+            horovod=args.horovod
+        )
+        
+        if tf.test.gpu_device_name():
+            print_fn = print_gpu_info
+        else:
+            print_fn = print_cpu_info
+        
+        if (args.horovod and hvd.rank() == 0) or not args.horovod:
+            print_fn()
+            print("\n\t\t\tStarting stabilizing epochs\t\t\t\n")
+        
+        # profiler.start()
+        generator, discriminator = train(
+            generator,
+            discriminator,
+            generator_optim,
+            discriminator_optim,
+            train_generator,
+            train_discriminator,
+            args.latent_dim,
+            dataset,
+            args.mixing_epochs,
+            args.stabilizing_epochs,
+            epoch_size,
+            batch_size,
+            args.training_ratio,
+            timestamp,
+            args.gradient_penalty_weight,
+            summary_writer,
+            is_mixing=False,
+            horovod=args.horovod
+        )
+        # profiler_result = profiler.stop()
+        # profiler.save(log_dir, profiler_result)
+        if (args.horovod and hvd.rank() == 0) or not args.horovod:
+            print_fn()
+            checkpoint_path = os.path.join('checkpoints', f'phase_{phase}')
+            if not os.path.exists(checkpoint_path):
+                os.makedirs(checkpoint_path)
+                
+            print(f"\n\nSaving weights to {checkpoint_path}.\n\n")
+                
+            generator.save_weights(os.path.join(checkpoint_path, 'generator.h5'), save_format='h5')
+            discriminator.save_weights(os.path.join(checkpoint_path, 'discriminator.h5'), save_format='h5')
+       
+        del generator, discriminator, generator_optim, discriminator_optim, train_generator, train_discriminator
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tfrecords_path', type=str)
+    parser.add_argument('num_phases', type=int)
+    parser.add_argument('--base_dim', type=int, default=128)
+    parser.add_argument('--latent_dim', type=int, default=128)
+    parser.add_argument('--training_ratio', type=int, default=1)
+    parser.add_argument('--gradient_penalty_weight', type=int, default=10)
+    parser.add_argument('--mixing_epochs', type=int, default=256)
+    parser.add_argument('--stabilizing_epochs', type=int, default=256)
+    parser.add_argument('--horovod', default=False, action='store_true')
+    args = parser.parse_args()
+    
+    if args.horovod:
+        hvd.init()
+        
+        if hvd.rank() == 0:
+            print(f"\n\n Using Horovod with global size {hvd.size()} and local size {hvd.local_size()}\n\n")
+        
+        print(hvd.rank())
+            
+    main(args)
